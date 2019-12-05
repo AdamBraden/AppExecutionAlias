885ce630f6d4c8e10f8631f62c996df1499289cf
Adam Braden <adambr@microsoft.com>
2019-12-05 11:30:35 -0800

    added projects

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = System.Environment.GetEnvironmentVariable("FOO");
+            if (!String.IsNullOrEmpty(value))
+            {
+                Console.WriteLine(value);
+            } else
+            {
+                Console.WriteLine("No FOO defined");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CreateProcessTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateProcessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startInfo = new System.Diagnostics.ProcessStartInfo();
+
+            startInfo.UseShellExecute = false;
+            startInfo.EnvironmentVariables["FOO"] = "Test";
+            startInfo.FileName = @"ConsoleApp6.exe";
+            startInfo.CreateNoWindow = false;
+
+            System.Diagnostics.Process.Start(startInfo).WaitForExit();
+            
+        }
+    }
+}
